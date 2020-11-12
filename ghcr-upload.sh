ba4d794844284096fedf20144fd00c8191927ddc
Daniel Freithofer <daniel.freithofer@trigo.at>
2020-11-12 12:05:55 +0100

    adding documentation, changing path from static to env var,

--- ghcr-upload.sh
+++ ghcr-upload.sh
@@ -1,5 +1,12 @@
 #!/bin/sh
 
-docker build . --tag ghcr.io/trigo-at/node-base:latest
+# builds image from repository Dockerfile and tags with repo name and branch as flag by extracting branch name from GITHUB_REF
+
+docker build . --tag ghcr.io/$GITHUB_REPOSITORY:${GITHUB_REF##*/}
+
+#logs into github container registry via secret token and username
+
 echo $TRIGO_PACKAGE_ACCESS_TOKEN | docker login ghcr.io -u $GITHUB_ACTOR --password-stdin
-docker push ghcr.io/trigo-at/node-base:latest
+
+#pushes image to github container registry
+docker push ghcr.io/$GITHUB_REPOSITORY:${GITHUB_REF##*/}
